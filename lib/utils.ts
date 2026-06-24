e3fe64746ec9506c22923c0d1584eeffe024b214
Emirhan Bayatlı <emirhan.bayatli99@icloud.com>
2026-06-24 12:11:21 +0300

    chore: update dependencies and add new packages for improved styling and functionality

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,9 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
 export function getErrorMessageFromCode(code: string): string {
   switch (code) {
     case "auth/user-not-found":
